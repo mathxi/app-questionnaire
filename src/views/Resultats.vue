0fcdfc45e445d20a0fd183e80af7ac7d42dbb6e5
mathxi <35033114+mathxi@users.noreply.github.com>
2019-11-12 23:54:31 +0100

    Fonctionnel manque la base de donnée (pouchdb)

--- src/views/Resultats.vue
+++ src/views/Resultats.vue
@@ -2,10 +2,11 @@
   <div class="resultats">
     <div class="center__elem">
       <h1>Vos résultats</h1>
-      <div class="md-subhead">Total questions: {{questionnaire.questions.length}}</div>
+      <div class="md-subhead">Total questions:{{nbGoodQuestions}} / {{questionnaire.questions.length}}</div>
       <md-card v-for="curQuestion in questionnaire.questions" v-bind:key="curQuestion.idQuestion">
         <md-card-actions>
-          <md-icon v-if="isQuestionRight(curQuestion)">check_circle</md-icon>
+          <md-icon class="trueValue" v-if="isQuestionRight(curQuestion)">check_circle</md-icon>
+          <md-icon class="falseValue" v-else>clear</md-icon>
         </md-card-actions>
         <CheckboxQuestion v-bind:disable="true" :question="curQuestion"></CheckboxQuestion>
       </md-card>
@@ -27,6 +28,15 @@ export default {
   name: "resultats",
   mounted() {},
   computed: {
+    nbGoodQuestions() {
+      var nbGood = 0;
+      this.questionnaire.questions.forEach(question => {
+        if (this.isQuestionRight(question)) {
+          nbGood++;
+        }
+      });
+      return nbGood;
+    },
     questionnaire: {
       get() {
         return this.$store.getters.getQuestionnaire(0);
@@ -45,17 +55,36 @@ export default {
       //les methodes find / map ect.. ne marchais pas
 
       console.log("question", curQuestion);
-      let nbGoodQuestion = 0;
+      let nbGoodAnswer = 0;
+      console.log("----------NOUVELLE QUESTION----------------");
+
       for (let index = 0; index != curQuestion.choices.length; index++) {
+        console.log("_________nouveau choix__________");
         let isTrue = curQuestion.trueAnswer.some(currentTrue => {
-          console.log("id egaux",curQuestion.choices[index].idChoice == currentTrue.idChoice)
-          console.log("valeurs egaux",curQuestion.choices[index].value == currentTrue.value)
+          console.log(
+            "id egaux",
+            curQuestion.choices[index].idChoice == currentTrue.idChoice
+          );
+          console.log(
+            "valeurs egaux",
+            curQuestion.choices[index].value == currentTrue.value
+          );
+          console.log("valeur choix= ", curQuestion.choices[index].value);
+          console.log("valeur correct= ", currentTrue.value);
           return (
             curQuestion.choices[index].idChoice == currentTrue.idChoice &&
             curQuestion.choices[index].value == currentTrue.value
           );
         });
         console.log("value find ", isTrue);
+        if (isTrue) {
+          nbGoodAnswer++;
+        }
+      }
+      if (nbGoodAnswer == curQuestion.choices.length) {
+        return true;
+      } else {
+        false;
       }
     }
   }
@@ -63,8 +92,11 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-.md-card i {
+.trueValue {
   color: green !important;
+}
+.falseValue {
+  color: red !important;
 }
 .md-card {
   width: 700px;
